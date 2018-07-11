2ffda23caafd872ed00bfd2369bf2db5dd2e1d68
AdrigomRoc <adrigomroc@gmail.com>
2018-07-11 12:45:38 +0200

    Començament

---
+++ apistarwars.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    
+    ?>
+    <select>
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+    </select>
+</body>
+</html>
